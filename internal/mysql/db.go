de2718eb69b3896a88c85423446bd236b9b4d2ab
Yin Xi <yinx@sea.com>
2022-06-13 16:40:09 +0800

    [SSO-1129] feat: add get db system variable function to support get current master

--- internal/mysql/db.go
+++ internal/mysql/db.go
@@ -9,7 +9,7 @@ import (
 )
 
 const (
-	stmtTimeout = 10 * time.Second
+	DDLTimeout = 10 * time.Second
 )
 
 type Conn struct {
@@ -19,7 +19,7 @@ type Conn struct {
 func (c Conn) CreateDB(dbName string) error {
 	stmt := "create database if not exists " + dbName
 	log.Debug().Str("create database stmt:", stmt).Msg("")
-	ctx, cancel := context.WithTimeout(context.Background(), stmtTimeout)
+	ctx, cancel := context.WithTimeout(context.Background(), DDLTimeout)
 	defer cancel()
 	if _, err := c.DB.ExecContext(ctx, stmt); err != nil {
 		return err
@@ -29,7 +29,7 @@ func (c Conn) CreateDB(dbName string) error {
 
 func (c Conn) CreateTab(dbName string) error {
 	stmt := "create table if not exists " + dbName + ".test_tab" + " (id int not null auto_increment primary key,col1 int not null, col2 char(20) not null)"
-	ctx, cancel := context.WithTimeout(context.Background(), stmtTimeout)
+	ctx, cancel := context.WithTimeout(context.Background(), DDLTimeout)
 	defer cancel()
 	if _, err := c.DB.ExecContext(ctx, stmt); err != nil {
 		return err
@@ -41,9 +41,25 @@ func (c Conn) Insert(dbName string, col1 int, col2 string) error {
 	defer internal.TimeTrack(time.Now())
 	funcName := internal.GetFuncName()
 	log.Debug().Str("func", funcName).Msg("")
+	master, err := c.GetSysVar("wsrep_node_name")
+	if err != nil {
+		return err
+	}
+	log.Debug().Str("master", *master).Msg("Current master")
 	stmt := "insert into " + dbName + ".test_tab (id, col1, col2) values (0, ?, ?)"
 	if _, err := c.DB.Exec(stmt, col1, col2); err != nil {
 		return err
 	}
 	return nil
+}
+
+func (c Conn) GetSysVar(sysvar string) (*string, error) {
+	stmt := "show global variables like " + "'" + sysvar + "'"
+	res := c.DB.QueryRow(stmt)
+	var key *string
+	var value *string
+	if err := res.Scan(&key, &value); err != nil {
+		return nil, err
+	}
+	return value, nil
 }
